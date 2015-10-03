12416e9521b015d9bc7eaeb4af0099058929be99
Bingfeng Liu <liubingfeng93@gmail.com>
2015-10-03 15:25:17 +1000

    added unit test for astar

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            AstarSearch astar = new AstarSearch();
+
+        }
+    }
+}
